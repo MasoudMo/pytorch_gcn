ae8ea091e2d448af09de0e253ce0fdfca77cc9d7
mmokhtari <mmokhtari@triumf.ca>
2021-05-01 22:42:18 -0700

    Removed test file

--- src/test.py
+++
@@ -1,8 +0,0 @@
-"""
-Author: Masoud Mokhtari
-
-This code is a PyTorch transalation of http://snap.stanford.edu/deepnetbio-ismb/ipynb/Graph+Convolutional+Prediction+of+Protein+Interactions+in+Yeast.html
-
-Model inference (edge prediction) on protein interactions in yeast
-
-"""
